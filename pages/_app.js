63445c02b16d6c972222dc1019cfa788e69b7969
Ucchhash <ucchhash@gmail.com>
2023-06-11 05:15:29 +0600

    added site icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,37 @@
-
+import Head from 'next/head';
 import React from 'react';
 import { IconContext } from 'react-icons';
 import Layout from '/components/Layout';
 import '/styles/globals.css';
 
-
 function MyApp({ Component, pageProps }) {
-
-
   return (
-    <IconContext.Provider value={{ color: "#b03a2e", className: "react-icons" }}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </IconContext.Provider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+        <link rel="icon" sizes="192x192" href="/icon.png"></link>
+        <link
+          rel="shortcut icon"
+          href="/icon.png"
+          type="image/png"
+        ></link>
+        <link
+          rel="apple-touch-icon"
+          href="/icon.png"
+          type="image/png"
+        ></link>
+        <title>Pizzaria</title>
+      </Head>
+      <IconContext.Provider value={{ color: "#b03a2e", className: "react-icons" }}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </IconContext.Provider>
+    </>
+
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,6 @@ import PizzaList from '../components/PizzaList';
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Pizzaria</title>
-        <meta name="description" content="Best Pizza Shop in Khulna" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <PizzaList />
     </>
   );
